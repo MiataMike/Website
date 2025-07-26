6b12a73410cd38e2d3c170749dcf930130fa8ff5
MiataMike <48374237+MiataMike@users.noreply.github.com>
2025-07-26 11:58:51 -0700

    added test page

--- dashboardWrapper.py
+++ dashboardWrapper.py
@@ -1,15 +1,17 @@
 from fastapi import FastAPI, APIRouter
 import subprocess
 import threading
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 router = APIRouter(prefix="/dashboard")
+app.mount("/dashboard", StaticFiles(directory="static/dashboard", html=True), name="dashboard")
 
 @app.get("/")
 def root():
     return {"status": "FastAPI server running"}
 
-@app.get("/run-benchtest")
+@router.post("/run-benchtest")
 def run_test():
     def target():
         subprocess.run(["python3", "BenchHost.py"])
