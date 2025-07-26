a7f3945483521465c38c8937d68a872d0a94d435
Mike Turner <mwtvy4@mst.edu>
2025-07-26 13:10:18 -0700

    serves json w.o. update

--- dashboardWrapper.py
+++ dashboardWrapper.py
@@ -1,21 +1,25 @@
 from fastapi import FastAPI, APIRouter
+from fastapi.responses import FileResponse
 import subprocess
 import threading
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
-router = APIRouter(prefix="/dashboard")
-app.mount("/", StaticFiles(directory="static/dashboard", html=True), name="dashboard")
+#router = APIRouter(prefix="/dashboard")
 
 #@app.get("/")
 #def root():
 #    return {"status": "FastAPI server running"}
 
-@router.post("/run-benchtest")
+@app.post("/run-benchtest")
 def run_test():
     def target():
         subprocess.run(["python3", "BenchHost.py"])
     threading.Thread(target=target).start()
     return {"msg": "Bench test started"}
 
-app.include_router(router)
+@app.get("/meta_stats.json")
+def get_stats():
+	return FileResponse("meta_stats.json")
+
+app.mount("/", StaticFiles(directory="static/dashboard", html=True), name="dashboard")
